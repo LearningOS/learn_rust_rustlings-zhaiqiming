653510fdcfce5468d10fa625ffb63b855b689e14
zhaiqiming <zhaiqimingac@outlook.com>
2022-07-07 00:42:28 +0800

    Progress: 31/84

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,7 +8,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+fn calculate_apple_price(num: usize) -> usize {
+    if num <= 40 {
+        num * 2
+    } else {
+        num
+    }
+}
 
 // Put your function here!
 // fn calculate_apple_price {
